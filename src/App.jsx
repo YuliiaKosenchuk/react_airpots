1d77cee5d03999cbbb9130aec2de54fb1eb90173
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-10-13 16:11:34 +0200

    emty project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import "./components/Header/Header";
+import Header from "./components/Header/Header";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
