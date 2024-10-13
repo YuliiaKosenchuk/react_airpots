5681df5228aec645616a8a0b8b03be3fc13e0b34
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-10-13 20:13:06 +0200

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,37 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./Header.scss";
-// import logoApple from "../../images/logo_apple.svg";
+import logoApple from "../../images/logo_apple.svg";
 
 function Header() {
-  //some//
- 
+
   return (
     <div className="header">
       <div className="container">
         <div className="header__top">
           <a href="#" className="logo">
-            {/* <img src={logoApple} alt="logo" /> */}
+            <img src={logoApple} alt="logo" />
           </a>
           <nav className="menu">
             <ul className="menu__list">
               <li className="menu__item">
-                <a href="#" className="menu__link">Info</a>
+                <a href="#" className="menu__link">
+                  Info
+                </a>
               </li>
               <li className="menu__item">
-                <a href="#" className="menu__link">Product</a>
+                <a href="#" className="menu__link">
+                  Product
+                </a>
               </li>
               <li className="menu__item">
-                <a href="#" className="menu__link">Contacts</a>
+                <a href="#" className="menu__link">
+                  Contacts
+                </a>
               </li>
               <li className="menu__item">
-                <button href="#" className="menu__bts">BUY</button>
+                <button href="#" className="menu__bts">
+                  BUY
+                </button>
               </li>
             </ul>
           </nav>
